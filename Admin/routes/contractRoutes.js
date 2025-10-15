dcb832b432d6c351718cf12a4d4e5276a8c4cf46
Samahsalah25 <samahsalah2555@gmail.com>
2025-10-15 04:58:01 +0300

    edit departments route

--- Admin/routes/contractRoutes.js
+++ Admin/routes/contractRoutes.js
@@ -16,7 +16,7 @@ const validate=require('../middlesware/validate');
 
 
 router.get('/', getContracts);
-router.get('/:id',authenticate,authorizeRoles('HR'), getContractById);
+router.get('/:id',authenticate,authorizeRoles('HR' ,'ADMIN'), getContractById);
 router.post('/',authenticate,authorizeRoles('ADMIN' ,'HR') ,validate(createContractSchema), createContract);
 router.patch('/:id' ,authenticate ,authorizeRoles('ADMIN' ,'HR'),validate(updateContractSchema),updateContract)
 router.delete('/:id',authenticate,authorizeRoles('HR' ,'HR'), deleteContract);
--- Admin/routes/departmentRoutes.js
+++ Admin/routes/departmentRoutes.js
@@ -10,9 +10,9 @@ const {
   updateDepartmentSchema,
 }=require('../validations/departmentValidation');
 
-router.post('/', authenticate,authorizeRoles('HR') ,validate(createDepartmentSchema),createDepartment);
+router.post('/', authenticate,authorizeRoles('HR','ADMIN') ,validate(createDepartmentSchema),createDepartment);
 router.get('/', getDepartments);
-router.get('/:id',authenticate,authorizeRoles('HR'), getDepartmentById);
-router.patch('/:id',authenticate,authorizeRoles('HR') ,validate(updateDepartmentSchema), updateDepartment);
-router.delete('/:id',authenticate,authorizeRoles('HR'), deleteDepartment);
+router.get('/:id',authenticate,authorizeRoles('HR','ADMIN'), getDepartmentById);
+router.patch('/:id',authenticate,authorizeRoles('HR','ADMIN') ,validate(updateDepartmentSchema), updateDepartment);
+router.delete('/:id',authenticate,authorizeRoles('HR','ADMIN'), deleteDepartment);
 module.exports = router;
