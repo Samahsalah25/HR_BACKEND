a7fb87af8811347f0cd103a3fd377a8dfa5cbac8
Samahsalah25 <samahsalah2555@gmail.com>
2025-11-20 22:18:31 +0200

    edit setcookies

--- utlis/setTokenCookie.js
+++ utlis/setTokenCookie.js
@@ -1,10 +1,13 @@
 const setTokenCookie = (res, token) => {
+  const isProduction = process.env.NODE_ENV === 'production';
+
   res.cookie('token', token, {
-    httpOnly: true,
-    secure: false,       // false على localhost
-    sameSite: 'lax',     // Lax كفاية للـ local
-    maxAge: 30 * 24 * 60 * 60 * 1000
+    httpOnly: true,             // يحمي الكوكي من JS
+    secure: isProduction,       // true في production, false في local
+    sameSite: isProduction ? 'none' : 'lax', // none في production, lax في local
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 يوم
   });
 };
+
 
 module.exports = setTokenCookie;
