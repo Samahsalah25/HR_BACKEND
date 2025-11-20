5383dea13035c7ea5294a350c549205f77f6f7a2
Samahsalah25 <samahsalah2555@gmail.com>
2025-11-20 17:40:22 +0200

    edit set cookies error

--- utlis/setTokenCookie.js
+++ utlis/setTokenCookie.js
@@ -1,8 +1,8 @@
 const setTokenCookie = (res, token) => {
   res.cookie('token', token, {
-    httpOnly: true,       // لازم يبقى httpOnly
-    secure: process.env.NODE_ENV === 'production',  // false لو local، true لو production
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', // None لو production، Lax لو local
+    // httpOnly: true,
+    secure: true,          // دايمًا true لأن Vercel دايمًا HTTPS
+    sameSite: 'none',      // دايمًا none علشان cross-origin (frontend + backend دومينات مختلفة)
     maxAge: 30 * 24 * 60 * 60 * 1000
   });
 };
