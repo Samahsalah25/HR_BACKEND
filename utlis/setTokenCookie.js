f89cda3a95c244c6da6193a7e52814648fb5d02c
Samahsalah25 <samahsalah2555@gmail.com>
2025-11-20 21:29:28 +0200

    edit cookies

--- utlis/setTokenCookie.js
+++ utlis/setTokenCookie.js
@@ -1,12 +1,12 @@
 const setTokenCookie = (res, token) => {
-res.cookie('token', token, {
-  httpOnly: true,  // شيلتيها مؤقتًا
-  secure: process.env.NODE_ENV === 'production',
-  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-  maxAge: 30 * 24 * 60 * 60 * 1000
-});
-
+  res.cookie('token', token, {
+    httpOnly: true, // يحمي الكوكي من الـ JS، خليها true
+    secure: process.env.NODE_ENV === 'production', // true على Vercel (HTTPS)، false لو local
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', // None على production، Lax على local
+    maxAge: 30 * 24 * 60 * 60 * 1000
+  });
 };
+
 
 
 module.exports = setTokenCookie;
