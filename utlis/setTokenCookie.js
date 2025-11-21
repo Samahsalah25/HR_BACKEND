2584af757777a844497c7b76ce1c0aae0a539454
Samahsalah25 <samahsalah2555@gmail.com>
2025-11-22 01:52:54 +0200

    edit setcookies6

--- utlis/setTokenCookie.js
+++ utlis/setTokenCookie.js
@@ -15,13 +15,20 @@
 const setTokenCookie = (res, token) => {
   const isProduction = process.env.NODE_ENV === "production";
 
+  // res.cookie("token", token, {
+  //   httpOnly: true,
+  //   secure: isProduction,              // Production = true / Local = false
+  //   sameSite: isProduction ? "none" : "lax",
+  //   maxAge: 15 * 60 * 1000,
+  // });
   res.cookie("token", token, {
-    httpOnly: true,
-    secure: isProduction,              // Production = true / Local = false
-    sameSite: isProduction ? "none" : "lax",
-    
-    maxAge: 15 * 60 * 1000,
-  });
+  httpOnly: true,
+  secure: true,               // HTTPS، موجود عندك
+  sameSite: "none",           // cross-domain
+  domain: ".hostingersite.com", // أي subdomain من Hostinger
+  maxAge: 15 * 60 * 1000,
+});
+
 };
 
 module.exports = setTokenCookie;
