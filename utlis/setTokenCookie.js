29be606975a9cd6a8ec189dd8f786105b7e99b87
Samahsalah25 <samahsalah2555@gmail.com>
2025-11-22 02:03:17 +0200

    edit setcookies6

--- utlis/setTokenCookie.js
+++ utlis/setTokenCookie.js
@@ -15,13 +15,18 @@
 const setTokenCookie = (res, token) => {
   const isProduction = process.env.NODE_ENV === "production";
 
-  res.cookie("token", token, {
+  // res.cookie("token", token, {
+  //   httpOnly: true,
+  //   secure: isProduction,              // Production = true / Local = false
+  //   sameSite: isProduction ? "none" : "lax",
+  //   maxAge: 15 * 60 * 1000,
+  // });
+   res.cookie("token", token, {
     httpOnly: true,
-    secure: isProduction,              // Production = true / Local = false
-    sameSite: isProduction ? "none" : "lax",
+    secure: isProduction,        // HTTPS مطلوب في production
+    sameSite: isProduction ? "lax" : "lax", // بدل none، لاقبلها أكثر على iOS
     maxAge: 15 * 60 * 1000,
   });
-   
 
 };
 
